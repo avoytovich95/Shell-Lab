a845b0def0167f8d29fa9c9931985dc764231ab1
avoytovich95 <darking271@gmail.com>
2017-11-30 11:26:25 -0500

    Everything

---
+++ cmake-build-debug/CMakeFiles/Lab4.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/cygdrive/c/Users/darki/Documents/School/2017-Fall/CSC-322/Lab4/myint.c" "/cygdrive/c/Users/darki/Documents/School/2017-Fall/CSC-322/Lab4/cmake-build-debug/CMakeFiles/Lab4.dir/myint.c.o"
+  "/cygdrive/c/Users/darki/Documents/School/2017-Fall/CSC-322/Lab4/myspin.c" "/cygdrive/c/Users/darki/Documents/School/2017-Fall/CSC-322/Lab4/cmake-build-debug/CMakeFiles/Lab4.dir/myspin.c.o"
+  "/cygdrive/c/Users/darki/Documents/School/2017-Fall/CSC-322/Lab4/mysplit.c" "/cygdrive/c/Users/darki/Documents/School/2017-Fall/CSC-322/Lab4/cmake-build-debug/CMakeFiles/Lab4.dir/mysplit.c.o"
+  "/cygdrive/c/Users/darki/Documents/School/2017-Fall/CSC-322/Lab4/mystop.c" "/cygdrive/c/Users/darki/Documents/School/2017-Fall/CSC-322/Lab4/cmake-build-debug/CMakeFiles/Lab4.dir/mystop.c.o"
+  "/cygdrive/c/Users/darki/Documents/School/2017-Fall/CSC-322/Lab4/tsh.c" "/cygdrive/c/Users/darki/Documents/School/2017-Fall/CSC-322/Lab4/cmake-build-debug/CMakeFiles/Lab4.dir/tsh.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Lab4.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab4.dir/myint.c.o"
+  "CMakeFiles/Lab4.dir/myspin.c.o"
+  "CMakeFiles/Lab4.dir/mysplit.c.o"
+  "CMakeFiles/Lab4.dir/mystop.c.o"
+  "CMakeFiles/Lab4.dir/tsh.c.o"
+  "Lab4.pdb"
+  "Lab4.exe"
+  "libLab4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lab4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
